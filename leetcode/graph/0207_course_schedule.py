6c8d390e2ac6655d63efeef4624ae503f3c97c43
David Luo <davidkluo@gmail.com>
2026-01-15 14:46:26 -0800

    Solve 207. Course Schedule with BFS

--- leetcode/graph/0207_course_schedule.py
+++ leetcode/graph/0207_course_schedule.py
@@ -1,8 +1,9 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
-    def can_finish_dfs(self, num_courses: int, prerequisites: List[List[int]]) -> bool:
+    def can_finish_dfs(self, num_courses: int, prerequisites: List[List[int]]
+                       ) -> bool:
         """
         Description:
         Given courses labeled from 0 to num_courses-1 and a list of prerequisite 
@@ -14,25 +15,25 @@ class Solution:
 
         graph = self.create_graph(prerequisites)
         confirmed = set() # courses confirmed to be possible
-        visiting = set() # currently visiting on dfs path
+        checking = set() # currently visiting on dfs path
 
         def dfs(course: int) -> bool:
             """ Cycle detection """
 
             if course in confirmed:
                 return True
-            elif course in visiting: # cycle detected
+            elif course in checking: # cycle detected
                 return False
             
-            visiting.add(course)
+            checking.add(course)
             
-            for neighbor in graph[course]:
-                if not dfs(neighbor): # cycle detected later, propagate up
+            for prereq in graph[course]:
+                if not dfs(prereq): # cycle detected later, propagate up
                     return False
             
             # no cycles detected
             confirmed.add(course)
-            visiting.remove(course)
+            checking.remove(course)
             return True
 
         for course in range(num_courses):
@@ -42,19 +43,55 @@ class Solution:
         return True
 
 
-    def can_finish_bfs(self, num_courses: int, prerequisites: List[List[int]]) -> bool:
+    def can_finish_bfs(self, num_courses: int, prerequisites: List[List[int]]
+                       ) -> bool:
+        graph = self.create_graph(prerequisites)
+        graph_r = self.create_reverse_graph(prerequisites)
+        indegrees = [0] * num_courses
+        num_completed = 0
+
+        for course in graph:
+            indegrees[course] = len(graph[course])
+
+        queue = deque()
+        for course in range(len(indegrees)):
+            if indegrees[course] == 0:
+                queue.append(course)
+                num_completed += 1
+
+        while queue:
+            course = queue.popleft()
+            for dependent in graph_r[course]:
+                indegrees[dependent] -= 1
+                if indegrees[dependent] == 0:
+                    queue.append(dependent)
+                    num_completed += 1
+        
+        return num_completed == num_courses
+            
+
+    def can_finish_topo(self, num_courses: int, prerequisites: List[List[int]]
+                        ) -> bool:
         pass
 
-    def can_finish_topo(self, num_courses: int, prerequisites: List[List[int]]) -> bool:
-        pass
-
-    def create_graph(self, prerequisites: List[List[int]]) -> dict[int, List[int]]:
+    def create_graph(self, prerequisites: List[List[int]]
+                     ) -> dict[int, List[int]]:
         graph = defaultdict(list)
 
         for course, prereq in prerequisites:
             graph[course].append(prereq)
 
         return graph
+    
+    def create_reverse_graph(self, prerequisites: List[List[int]]
+                             ) -> dict[int, List[int]]:
+        reverse_graph = defaultdict(list)
+
+        for course, prereq in prerequisites:
+            reverse_graph[prereq].append(course)
+        
+        return reverse_graph
+        
         
 
 if __name__ == "__main__":
@@ -62,7 +99,7 @@ if __name__ == "__main__":
     # test cases
     functions = [
         s.can_finish_dfs,
-        # s.can_finish_bfs,
+        s.can_finish_bfs,
         # s.can_finish_topo
     ]
     for func in functions:
